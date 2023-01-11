e8905352dc1ea866fc0de594604c65f3bcb9f0d7
Vladislav Patrakov <gostreid1@gmail.com>
2023-01-11 15:54:48 +0300

    develop 10

--- YA Alice and the Secret.playground/Contents.swift
+++ YA Alice and the Secret.playground/Contents.swift
@@ -8,6 +8,7 @@ func readGameConfig() -> Int {
         fatalError()
     }
     return input[0]
+
 }
 
 func ask(date: String) {
@@ -20,11 +21,51 @@ func print(date: String) {
     fflush(stdout)
 }
 
+let start = "1970-01-01T10:44:00+0000"
+let end = "2020-12-31T10:44:00+0000"
+let dateFormatter = ISO8601DateFormatter()
+let dateStart = dateFormatter.date(from:start)!
+let dateEnd = dateFormatter.date(from:end)!
+
+
+let timeIntervalStart = dateStart.timeIntervalSince1970
+let timeIntervalEnd = dateEnd.timeIntervalSince1970
+var dateStarInt = Int(timeIntervalStart)
+var dateEndInt = Int(timeIntervalEnd)
+
+
 func solution() {
     // read game configuration
     let _ = readGameConfig()
 
     // write your code here
+    
+    
+    ask(date: "31.12.2020")
+    let answer = readGameConfig()
+    let memorableDateNumber = answer / 2 + 1
+    
+    let dayTimePeriodFormatter = DateFormatter()
+    dayTimePeriodFormatter.dateFormat = "dd.MM.YYYY"
+    var selectedDate = ""
+    var total = 0
+    
+    while dateStarInt != dateEndInt && total != 18 {
+        let middleDate = (dateStarInt + dateEndInt) / 2
+        let date = Date(timeIntervalSince1970: TimeInterval(middleDate))
+        
+        selectedDate = dayTimePeriodFormatter.string(from: date)
+        ask(date: selectedDate)
+        let answerSys = readGameConfig()
+        
+        if  answerSys >= memorableDateNumber {
+            dateEndInt = middleDate
+        } else {
+            dateEndInt = middleDate
+        }
+        total += 1
+    }
+    print(date: selectedDate)
 }
 
 solution()
